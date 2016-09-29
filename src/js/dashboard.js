4d6150aa4831b67ba4f03bd59844672b39ddd526
Shahidh K Muhammed <shahidh@hasura.io>
2016-09-29 10:29:00 +0530

    Cookie test

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -45,11 +45,13 @@ $('#nav').affix({
       // And send the token over to the server
       var url = 'https://auth.emergency-108-hackathon.hasura-app.io/google/authenticate?access_token='
         + params['access_token'];
-      $.get(url, function(response) {
-        USER = response;
-        loadTeam();
-        console.log(response);
-      });
+        window.location = url;
+      //
+      // $.get(url, function(response) {
+      //   USER = response;
+      //   loadTeam();
+      //   console.log(response);
+      // });
     }
 
     function loadUser(){
