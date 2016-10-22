3e177c95ab147c7ce659c397bd09958a745a0bb9
Shahidh K Muhammed <shahidh@hasura.io>
2016-10-22 11:41:49 +0530

    Add toast

---
+++ src/js/toast.js
@@ -0,0 +1,49 @@
+//toast.js
+$.getScript("//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js", function(){
+
+  toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+
+  var toasts = [
+    {
+      type: 'success',
+      message: 'Submissions are now open!'
+    },
+    {
+      type: 'info',
+      message: 'Schedule has been updated!'
+    }
+  ]
+  var i;
+  i=0;
+  function nextI (toasts) {
+    if  (i>=toasts.length-1) {
+      i = 0;
+    } else {
+      i += 1;
+    }
+    return toasts[i];
+  }
+  var toast = nextI(toasts);
+  toastr[toast.type](toast.message);
+  setInterval(function () {
+    var toast = nextI(toasts);
+    toastr[toast.type](toast.message);
+  }, 5500);
+
+});
